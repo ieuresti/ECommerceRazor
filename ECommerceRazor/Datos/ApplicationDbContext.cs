f8847ea0a49f44b29edb1a33d4cba7436b8eb4f2
Ivan Uresti <talyfuantos@gmail.com>
2025-12-16 12:21:53 -0600

    Agregar archivos de proyecto.

---
+++ ECommerceRazor/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceRazor.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets de las entidades de la aplicación
+
+    }
+}
